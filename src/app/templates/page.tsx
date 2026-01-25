4b2ff2d03b011cb11f50fa222282647ee3f1bd3d
kiwito200-wq <kiwito200@gmail.com>
2026-01-25 17:54:54 +0100

    Add mark-all-read icon for notifications

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -437,9 +437,7 @@ export default function TemplatesPage() {
                         }}
                         className="text-gray-400 hover:text-gray-600"
                       >
-                        <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                        </svg>
+                        <img src="/icons/mark-all-read.svg" alt="" className="w-5 h-5" />
                       </button>
                     </Tooltip>
                   </div>
